dad8903d3a3058dc6607aac7551c6e126bad5879
Udathu.Hari Hara Durga Vasisht Pranav <102406736+HHDVasishtPranav@users.noreply.github.com>
2024-06-24 12:59:30 +0530

    Update autoCommit.sh

--- autoCommit.sh
+++ autoCommit.sh
@@ -1,4 +1,4 @@
-cd path
+cd path/of/the/repo
 
 git add .
 
@@ -8,5 +8,5 @@ git commit -m "$commit_message"
 
 git push origin main
 
-echo "Committed and pushed on $(date +"%Y-%m-%d %H:%M:%S")" >> path/commitlog.txt
+echo "Committed and pushed on $(date +"%Y-%m-%d %H:%M:%S")" >> path/of/the/repo/commitlog.txt
 
